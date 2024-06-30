fe746f1db249f5b0204f098dfabc663d222835e0
박소현[Papago Web] <sohyun.grace.park@navercorp.com>
2024-06-30 22:48:21 +0900

    fix     : 최근 데이터 누락인 경우 안내문 노출

--- src/app/search/[searchKey]/page.tsx
+++ src/app/search/[searchKey]/page.tsx
@@ -29,7 +29,7 @@ export default function SearchPage({
 }: {
   params: { searchKey: string };
 }) {
-  const [data, setData] = useState<ItemSummary[]>([]);
+  const [data, setData] = useState<ItemSummary[] | null>(null);
   const searchKey = decodeURIComponent(params.searchKey);
 
   useEffect(() => {
@@ -42,91 +42,112 @@ export default function SearchPage({
 
   return (
     <>
-      <Heading as="h2" size="md" mt={10}>
-        <Text as="span" color="brand.500">
-          {searchKey}
-        </Text>{" "}
-        키워드로 검색한&nbsp;
-        <Text as="span" color="brand.500">
-          {data.length}개
-        </Text>
-        의 결과입니다.
-      </Heading>
-      <Skeleton isLoaded={data.length > 0} height={48}>
+      {data && (
+        <Heading as="h2" size="md" mt={10}>
+          {data.length > 0 ? (
+            <>
+              <Text as="span" color="brand.500">
+                {searchKey}
+              </Text>{" "}
+              키워드로 검색한&nbsp;
+              <Text as="span" color="brand.500">
+                {data.length}개
+              </Text>
+              의 결과입니다.
+            </>
+          ) : (
+            <>
+              <Text as="span" color="brand.500">
+                {searchKey}
+              </Text>
+              로 검색한 최근 10일까지의 데이터가 없습니다.
+            </>
+          )}
+        </Heading>
+      )}
+      <Skeleton isLoaded={data !== null} height={48}>
         <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={4} mt={6}>
-          {data.map((info, index) => (
-            <Box key={index} p={6} bgColor="white" rounded="lg" boxShadow="md">
-              <Link
-                href={`/detail/${info.itemName}`}
-                className={`${styles.item_info} ${
-                  styles[`type_${info.itemCode}`]
-                }`}
-                mb={4}
-                display="block"
+          {data &&
+            data.length > 0 &&
+            data.map((info, index) => (
+              <Box
+                key={index}
+                p={6}
+                bgColor="white"
+                rounded="lg"
+                boxShadow="md"
               >
-                <Heading as="h3" size="md">
-                  {info.itemName} ({info.rank})
-                </Heading>
-                <Text fontSize="sm" color="gray.500">
-                  {info.kindName}
-                </Text>
-              </Link>
-              <Box mb={4}>
-                <Text fontSize="xl" fontWeight="700" color="brand.500">
-                  {`${info.itemPrice}원`}
-                </Text>
-                {/* TODO: API에 가격이 없음. info.item_price.toLocaleString() */}
-                <Text color="gray.500" fontWeight="700">
-                  {info.diffWeekAgo > 0 && (
-                    <>
-                      일주일 전보다&nbsp;
-                      <Text as="span" color="red.500">
-                        {info.diffWeekAgo.toLocaleString()}원&nbsp;
-                        <Icon
-                          as={TrendingUp}
-                          boxSize={7}
-                          verticalAlign="middle"
-                        />
-                      </Text>
-                    </>
-                  )}
-                  {info.diffWeekAgo < 0 && (
-                    <>
-                      일주일 전보다&nbsp;
-                      <Text as="span" color="blue.500">
-                        {info.diffWeekAgo.toLocaleString()}원&nbsp;
-                        <Icon
-                          as={TrendingDown}
-                          boxSize={7}
-                          verticalAlign="middle"
-                        />
-                      </Text>
-                    </>
-                  )}
-                  {info.diffWeekAgo === 0 && (
-                    <Text as="span">일주일 전과 가격이 동일합니다.</Text>
-                  )}
-                </Text>
-              </Box>
-              <Flex justifyContent="space-between" alignItems="center">
-                <Text fontSize="sm" color="gray.500">
-                  {new Date(info.searchDate).toLocaleDateString("ko-KR", {
-                    year: "2-digit",
-                    month: "numeric",
-                    day: "numeric",
-                  })}{" "}
-                  / 1kg 기준
-                </Text>
-                <Button
-                  as={Link}
-                  href={`/detail/?itemCode=${info.itemCode}&date=${info.searchDate}`}
-                  variant="brand_light"
+                <Link
+                  href={`/detail/${info.itemName}`}
+                  className={`${styles.item_info} ${
+                    styles[`type_${info.itemCode}`]
+                  }`}
+                  mb={4}
+                  display="block"
                 >
-                  자세히 보기
-                </Button>
-              </Flex>
-            </Box>
-          ))}
+                  <Heading as="h3" size="md">
+                    {info.itemName} ({info.rank})
+                  </Heading>
+                  <Text fontSize="sm" color="gray.500">
+                    {info.kindName}
+                  </Text>
+                </Link>
+                <Box mb={4}>
+                  <Text fontSize="xl" fontWeight="700" color="brand.500">
+                    {`${info.itemPrice}원`}
+                  </Text>
+                  {/* TODO: API에 가격이 없음. info.item_price.toLocaleString() */}
+                  <Text color="gray.500" fontWeight="700">
+                    {info.diffWeekAgo > 0 && (
+                      <>
+                        일주일 전보다&nbsp;
+                        <Text as="span" color="red.500">
+                          {info.diffWeekAgo.toLocaleString()}원&nbsp;
+                          <Icon
+                            as={TrendingUp}
+                            boxSize={7}
+                            verticalAlign="middle"
+                          />
+                        </Text>
+                      </>
+                    )}
+                    {info.diffWeekAgo < 0 && (
+                      <>
+                        일주일 전보다&nbsp;
+                        <Text as="span" color="blue.500">
+                          {info.diffWeekAgo.toLocaleString()}원&nbsp;
+                          <Icon
+                            as={TrendingDown}
+                            boxSize={7}
+                            verticalAlign="middle"
+                          />
+                        </Text>
+                      </>
+                    )}
+                    {info.diffWeekAgo === 0 && (
+                      <Text as="span">일주일 전과 가격이 동일합니다.</Text>
+                    )}
+                  </Text>
+                </Box>
+                <Flex justifyContent="space-between" alignItems="center">
+                  <Text fontSize="sm" color="gray.500">
+                    {new Date(info.searchDate).toLocaleDateString("ko-KR", {
+                      year: "2-digit",
+                      month: "numeric",
+                      day: "numeric",
+                    })}{" "}
+                    / 1kg 기준
+                  </Text>
+                  <Button
+                    as={Link}
+                    href={`/detail/?itemCode=${info.itemCode}&date=${info.searchDate}`}
+                    variant="brand_light"
+                  >
+                    자세히 보기
+                  </Button>
+                </Flex>
+              </Box>
+            ))}
         </SimpleGrid>
       </Skeleton>
     </>
