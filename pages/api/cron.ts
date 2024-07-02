58e5e2733dba37ec3939e8f46fb64c76e9572441
박소현[Papago Web] <sohyun.grace.park@navercorp.com>
2024-07-02 22:22:55 +0900

    fix     : 크론 헤더 조회 오류 수정

---
+++ pages/api/cron.ts
@@ -0,0 +1,24 @@
+import { fill_price_one_day_data } from "../../src/fill-db";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { format } from "date-fns";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const authHeader = request.headers["authorization"];
+  if (
+    !process.env.CRON_SECRET ||
+    authHeader !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return response.status(401).json({ success: false });
+  }
+
+  const stringDate = format(new Date(), "yyyy-MM-dd");
+  try {
+    await fill_price_one_day_data(stringDate);
+    response.status(200).end("Hello Cron!");
+  } catch (err) {
+    response.status(400).end(`${stringDate} data error: ${err}`);
+  }
+}
